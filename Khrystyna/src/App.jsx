1e842435ced823d07bb2c379e4bfae3ca507b12d
Eridana <hrechkochristin@gmail.com>
2026-07-01 12:10:24 +0300

    Eridana_1.0 (Khrystyna's Vite project creation)

---
+++ Khrystyna/src/App.jsx
@@ -0,0 +1,156 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import altrinLogo from './assets/altrin.jpg'
+import './App.css'
+
+import Header from './components/Header.jsx'
+
+export default function App(){
+  return (
+    <>
+      <div className="StartPage">
+        <Header />
+        <div className="Center">
+          <h1 className="AppName">Altrin</h1>
+          <img src={altrinLogo} className="Logo" alt="Altrin logo" width="200" height="200" />
+          <h2 className="AppDescription">A platform for learning and sharing knowledge</h2>
+        </div>
+
+        <div className="infoBlock">
+          <h3 className="title">
+            It is a place where you can find and share knowledge with others.
+          </h3>
+
+          <p className="text">
+            You can find courses, tutorials, and articles on various topics. You can
+            also share your own knowledge by creating courses, tutorials, and
+            articles.
+          </p>
+        </div>
+
+        <div className="authButtons">
+          <h3 className="AppDescription">Get started by logging in or signing up</h3>
+          <button type="button" className="button">Log in</button>
+          <button type="button" className="button">Sign up</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <section id="center">
+//         <div className="hero">
+//           <img src={heroImg} className="base" width="170" height="179" alt="" />
+//           <img src={reactLogo} className="framework" alt="React logo" />
+//           <img src={viteLogo} className="vite" alt="Vite logo" />
+//         </div>
+//         <div>
+//           <h1>Get started</h1>
+//           <p>
+//             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+//           </p>
+//         </div>
+//         <button
+//           type="button"
+//           className="counter"
+//           onClick={() => setCount((count) => count + 1)}
+//         >
+//           Count is {count}
+//         </button>
+//       </section>
+
+//       <div className="ticks"></div>
+
+//       <section id="next-steps">
+//         <div id="docs">
+//           <svg className="icon" role="presentation" aria-hidden="true">
+//             <use href="/icons.svg#documentation-icon"></use>
+//           </svg>
+//           <h2>Documentation</h2>
+//           <p>Your questions, answered</p>
+//           <ul>
+//             <li>
+//               <a href="https://vite.dev/" target="_blank">
+//                 <img className="logo" src={viteLogo} alt="" />
+//                 Explore Vite
+//               </a>
+//             </li>
+//             <li>
+//               <a href="https://react.dev/" target="_blank">
+//                 <img className="button-icon" src={reactLogo} alt="" />
+//                 Learn more
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//         <div id="social">
+//           <svg className="icon" role="presentation" aria-hidden="true">
+//             <use href="/icons.svg#social-icon"></use>
+//           </svg>
+//           <h2>Connect with us</h2>
+//           <p>Join the Vite community</p>
+//           <ul>
+//             <li>
+//               <a href="https://github.com/vitejs/vite" target="_blank">
+//                 <svg
+//                   className="button-icon"
+//                   role="presentation"
+//                   aria-hidden="true"
+//                 >
+//                   <use href="/icons.svg#github-icon"></use>
+//                 </svg>
+//                 GitHub
+//               </a>
+//             </li>
+//             <li>
+//               <a href="https://chat.vite.dev/" target="_blank">
+//                 <svg
+//                   className="button-icon"
+//                   role="presentation"
+//                   aria-hidden="true"
+//                 >
+//                   <use href="/icons.svg#discord-icon"></use>
+//                 </svg>
+//                 Discord
+//               </a>
+//             </li>
+//             <li>
+//               <a href="https://x.com/vite_js" target="_blank">
+//                 <svg
+//                   className="button-icon"
+//                   role="presentation"
+//                   aria-hidden="true"
+//                 >
+//                   <use href="/icons.svg#x-icon"></use>
+//                 </svg>
+//                 X.com
+//               </a>
+//             </li>
+//             <li>
+//               <a href="https://bsky.app/profile/vite.dev" target="_blank">
+//                 <svg
+//                   className="button-icon"
+//                   role="presentation"
+//                   aria-hidden="true"
+//                 >
+//                   <use href="/icons.svg#bluesky-icon"></use>
+//                 </svg>
+//                 Bluesky
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </section>
+
+//       <div className="ticks"></div>
+//       <section id="spacer"></section>
+//     </>
+//   )
+// }
---
+++ Khrystyna/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+  return (
+    <header className="header">  
+      <nav>Menu</nav>
+      <h3>Altrin</h3>
+      <span>13:45</span>
+    </header>
+  )
+}
